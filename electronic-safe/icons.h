cf80ffaac908d378476bb426ebbfe21adad0a030
Marina Zelinskaya <95147513+baka314@users.noreply.github.com>
2023-11-15 10:26:46 +0200

    uploading the project

---
+++ electronic-safe/icons.h
@@ -0,0 +1,16 @@
+
+#ifndef ICONS_H
+#define ICONS_H
+
+#include <LiquidCrystal.h>
+
+// Our custom icon numbers
+#define ICON_LOCKED_CHAR   (byte)0
+#define ICON_UNLOCKED_CHAR (byte)1
+
+// This is a standard icon on the LCD1602 character set
+#define ICON_RIGHT_ARROW   (byte)126
+
+void init_icons(LiquidCrystal &lcd);
+
+#endif /* ICONS_H */
